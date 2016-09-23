c2d0db844e67b9e64dcd37bab58153ba9ec3dec2
MrTuo <595983351@qq.com>
2016-09-23 18:23:21 +0800

    修改处理幂乘的bug

--- experiment_1/src/experiment_1/experiment_1.java
+++ experiment_1/src/experiment_1/experiment_1.java
@@ -1,3 +1,4 @@
+
 package experiment_1;
 
 import java.util.ArrayList;
@@ -7,20 +8,20 @@ import java.util.regex.Pattern;
 
 public class experiment_1 {
 
-	//letter numbser新表达式�?要初始化
+	//letter numbser新表达式需要初始化
 	public static void  main (String[] args){
 		//variable
 		@SuppressWarnings("resource")
 		Scanner in=new Scanner(System.in);
-		String expression=null;//输入的表达式或�?�是！命令行
+		String expression=null;//输入的表达式或者是！命令行
 		String end_expression=null;//这里存放的是表达式，当expression输入的是正确的表达式时，将end_expression更新
 		String[] xiang;//这里存放的是将表达式分解为项的String数组
-		boolean judge=false;//判断表达式是否正�?
+		boolean judge=false;//判断表达式是否正确
 		int choice=0;//选择
-		ArrayList<String>  number = new ArrayList<String> ();//记录赋�?�函数的�?
-		ArrayList<String>  letter = new ArrayList<String> ();//赋�?�函数的类型
-		ArrayList<String>  fuhao = new ArrayList<String> ();//存放表达式中�?+号或者是-号的位置顺序
-		char VarDerivative = 0;//保证求导变量的类�?
+		ArrayList<String>  number = new ArrayList<String> ();//记录赋值函数的值
+		ArrayList<String>  letter = new ArrayList<String> ();//赋值函数的类型
+		ArrayList<String>  fuhao = new ArrayList<String> ();//存放表达式中的+号或者是-号的位置顺序
+		char VarDerivative = 0;//保证求导变量的类型
 		int j=0;
 		int x=0;
 		int y=0;
@@ -28,14 +29,14 @@ public class experiment_1 {
 		while(true){
 			choice=0;
 			expression=in.nextLine();//输入
-			fuhao=myfuhao(expression);//获得输入的符号，不管是不是正确的表达式或者是赋�?�求导指�?
-			expression=change(expression,fuhao);//将表达式转变为标准的表达式（无论表达式是否正确或者是输入的赋值求导指�?
+			fuhao=myfuhao(expression);//获得输入的符号，不管是不是正确的表达式或者是赋值求导指令
+			expression=change(expression,fuhao);//将表达式转变为标准的表达式（无论表达式是否正确或者是输入的赋值求导指令
 
-			judge=expression(expression, expression.length());//判断表达式是否正�?
-			if (expression.length()>=9 && expression.substring(0, 9).equals("!simplify")){//如果是赋值�?�择2
+			judge=expression(expression, expression.length());//判断表达式是否正确
+			if (expression.length()>=9 && expression.substring(0, 9).equals("!simplify")){//如果是赋值选择2
 				choice=2;
 			}
-			else if(expression.length()>=4 && expression.substring(0, 4).equals("!d/d")){//如果是求导�?�择3
+			else if(expression.length()>=4 && expression.substring(0, 4).equals("!d/d")){//如果是求导选择3
 				choice=3;
 			}
 			else{
@@ -45,7 +46,7 @@ public class experiment_1 {
 			{
 			case 1:
 				if (judge){//如果表达式正确，则输出表达式，并更新end_expression
-					System.out.println("改变后的表达�?:"+expression);
+					System.out.println("转变后"+expression);
 					end_expression=expression;
 				}
 				else{//不正确则输出错误信息
@@ -55,28 +56,28 @@ public class experiment_1 {
 				break;
 			case 2:
 				if (end_expression==null){
-					System.out.println("Error There is no expression!");//在没有表达式的情况下无法赋�??
+					System.out.println("Error There is no expression!");//在没有表达式的情况下无法赋值
 				}
 				else{
 					for (int i=expression.length()-1;i>=0;i--){//获得要赋值的变量
 						if(expression.charAt(i)=='='){
 							j=i;
-							number.add(expression.substring(i+1,j+2));//获得要赋值变量的�?
+							number.add(expression.substring(i+1,j+2));//获得要赋值变量的值
 						}
 						else if(expression.charAt(i)==' '){
 							letter.add(expression.substring(i+1, j));//获得要赋值变量的名称
 						}
 					}
-					simplify(end_expression,letter,number);//计算赋�?�后的结果，在函数中输出
+					simplify(end_expression,letter,number);//计算赋值后的结果，在函数中输出
 				}
 				break;
 			case 3:
 				if(end_expression==null){
 					System.out.println("Error There is no expression!");//在没有表达式的情况下无法求导
 				}
-				else{//有了表达�?
-					//首先要将求导的变量求出来 变量只能是在 !d/d()括号里面的�?? "()"是不能存在的 且变量只能有字母组成
-					VarDerivative=expression.charAt(4);//获得要求导的变量 这里变量只能是单个字�?
+				else{//有了表达式
+					//首先要将求导的变量求出来 变量只能是在 !d/d()括号里面的值 "()"是不能存在的 且变量只能有字母组成
+					VarDerivative=expression.charAt(4);//获得要求导的变量 这里变量只能是单个字符
 					j=0;
 					/*System.out.println(VarDerivative);
 					for (int i=0;i<expression.length();i++){
@@ -95,7 +96,7 @@ public class experiment_1 {
 					xiang=splitby_jia(end_expression);
 					//System.out.println(1);
 					fuhao=myfuhao(end_expression);
-					derivative(xiang,VarDerivative,end_expression,fuhao);//输出求导后的结果，在函数中输�?
+					derivative(xiang,VarDerivative,end_expression,fuhao);//输出求导后的结果，在函数中输出
 				}
 				break;
 			default:
@@ -105,7 +106,7 @@ public class experiment_1 {
 			 
 		}
 	}
-	public static boolean expression(String str,int len){//输入表达�?
+	public static boolean expression(String str,int len){//输入表达式
 		char a=0,b=0,c=0;//b is behind a,c is in front of a
 		int x=0;//judge the number is the first one or not
 		for (int i=0;i<len;i++){;
@@ -148,7 +149,7 @@ public class experiment_1 {
 		} 
 		return true;
 	}
-	public static void simplify(String expression,ArrayList<String> letter,ArrayList<String> number){//初始化赋�?
+	public static void simplify(String expression,ArrayList<String> letter,ArrayList<String> number){//初始化赋值
 		String end=null;
 		end =expression;
 			/*for (int i=0;i<letter.size();i++){
@@ -160,17 +161,17 @@ public class experiment_1 {
 		}
 		System.out.println("Before make simple");
 		System.out.println(end);
-		//将化�?结果合并�?�? by�?
-		ArrayList<String> newItem = getItems(end);//获取多项�?
-		int[] newSign = getSign(end, newItem);//获取多项式符号数�?
+		//将化简结果合并简化 by妥
+		ArrayList<String> newItem = getItems(end);//获取多项式
+		int[] newSign = getSign(end, newItem);//获取多项式符号数组
 		String newExpression = makeSimple(end, newItem, newSign);
 		System.out.println("After make simple");
 		System.out.println(newExpression);
 		//return end;
 	}
 	@SuppressWarnings("null")
-	public static String derivative(String[] itemCount,char var,String expression,ArrayList<String> fuhao){//求导初始�?
-		int varNum[];//变量的个�?
+	public static String derivative(String[] itemCount,char var,String expression,ArrayList<String> fuhao){//求导初始化
+		int varNum[];//变量的个数
 		int xishu[];//项的系数
 		ArrayList<String>  newitemCount = new ArrayList<String> ();
 		ArrayList<String>  newfuhao = new ArrayList<String> ();
@@ -184,14 +185,14 @@ public class experiment_1 {
 		}
 		else{
 			for (int i=0;i<itemCount.length;i++){
-				for (int j=0;j<itemCount[i].length();j++){//计算变量的个�?
+				for (int j=0;j<itemCount[i].length();j++){//计算变量的个数
 					if(var==itemCount[i].charAt(j)){
 						varNum[i]++;
 					}
 				}
 			}
 
-			for (int i=0;i<itemCount.length;i++){//合成求导后的�?
+			for (int i=0;i<itemCount.length;i++){//合成求导后的项
 				if(varNum[i]!=0){
 					if(i<itemCount.length-1){
 						//System.out.println(fuhao);
@@ -213,20 +214,20 @@ public class experiment_1 {
 			}
 		}
 		
-		//将化�?结果合并�?�? by�?
+		//将化简结果合并简化 by妥
 		System.out.println("Before make simple");
 		System.out.println(end);
-		ArrayList<String> newItem = getItems(end);//获取多项�?
-		int[] newSign = getSign(end, newItem);//获取多项式符号数�?
+		ArrayList<String> newItem = getItems(end);//获取多项式
+		int[] newSign = getSign(end, newItem);//获取多项式符号数组
 		String newExpression = makeSimple(end, newItem, newSign);
 		System.out.println("After make simple");
 		System.out.println(newExpression);
 		return newExpression;
 	}
-	public static String[] splitby_jia(String str){//按加法分�?
+	public static String[] splitby_jia(String str){//按加法分割
 		return str.split("\\+|\\-");
 	}
-	public static String[] splitby_cheng(String str){//按乘法分�?
+	public static String[] splitby_cheng(String str){//按乘法分割
 		return str.split("\\*");
 	}
 	public static ArrayList<String> myfuhao(String end_expression){
@@ -241,16 +242,17 @@ public class experiment_1 {
 		}
 		return fuhao;
 	}
-	public static String change(String expression,ArrayList<String> fuhao){//将输入的表达式进行修�?
+	public static String change(String expression,ArrayList<String> fuhao){//将输入的表达式进行修改
 		String change_expression=null;
 		String end_expression="";
 		String[] str;
 		String[] xiang;
+		String newstr = null;
 		String newxiang = null;
 		int xishu=0;
-		int fuhao1=0;//标记到第几个符号�?
+		int fuhao1=0;//标记到第几个符号了
 		change_expression=expression.replace("\t", "");//将tab删掉
-		change_expression=change_expression.replace(" ", "");//将空格删�?
+		change_expression=change_expression.replace(" ", "");//将空格删掉
 
 		//System.out.println(str+" ");
 		for (int i=0;i<change_expression.length();i++){
@@ -266,12 +268,13 @@ public class experiment_1 {
 		}
 		end_expression=change_expression;
 		str=end_expression.split("\\+|\\-");
-		System.out.println("输入的表达式:"+end_expression);
-		for (int i=0;i<str.length;i++){//依次访问每个�?
+//		System.out.println(end_expression);
+		for (int i=0;i<str.length;i++){//依次访问每个项
+			newstr=str[i];
 			xiang=str[i].split("\\*");
 			for(int j=0;j<xiang.length;j++){//对每个项访问
 				newxiang=xiang[j];
-//				System.out.println("newxiang1"+"   "+newxiang);
+				//System.out.println("newxiang1"+"   "+newxiang);
 				for(int k=0;k<xiang[j].length();k++){
 					if(xiang[j].charAt(k)=='^'){
 						for (int l=k+1;l<xiang[j].length();l++){//计算^后面的系数是多少
@@ -283,7 +286,7 @@ public class experiment_1 {
 							}
 						}
 						newxiang=newxiang.substring(0, newxiang.indexOf("^"));
-//						System.out.println("newxiang2"+"   "+newxiang);
+//						System.out.println("newxiang2"+"   "+xiang[j]);
 //						System.out.println("newxiang3"+"   "+newxiang);
 						for (int m=0;m<xishu-1;m++){
 							newxiang+="*"+xiang[j].substring(0, xiang[j].indexOf("^"));
@@ -293,6 +296,7 @@ public class experiment_1 {
 					}
 				}
 			}
+			xishu=0;
 //			System.out.println("1=============");
 //			for (int n=0;n<xiang.length;n++){
 //				System.out.println(xiang[n]);
@@ -303,20 +307,16 @@ public class experiment_1 {
 				newxiang+="*"+xiang[n];
 			}
 			str[i]=newxiang;
-//			System.out.println("*****"+str[i]);
+//			System.out.println(str[i]);
 		}
-//		System.out.println("fuhao"+"   "+fuhao);
-//		System.out.println("str"+"   "+str.length);
+	//	System.out.println("fuhao"+"   "+fuhao);
+	//	System.out.println("str"+"   "+str.length);
 		//每个项都处理完了，该合并项了
 		end_expression="";
-//		for (int i=0;i<str.length;i++){
-//			System.out.println("str"+"   "+str[i]);
-//		}
 		for (int i=0;i<str.length;i++){
 			end_expression+=str[i];
 			if(i<str.length-1){
-				end_expression+=fuhao.get(fuhao1);
-				fuhao1++;
+				end_expression+=fuhao.get(fuhao1++);
 			}
 //			System.out.println("end_expression"+"   "+end_expression);
 		}
@@ -325,7 +325,7 @@ public class experiment_1 {
 	
     /**
      * 对输入的expression，按加号拆分为多个多想式，保存并返回
-     * 保存为ArrayList类型的优势在于在化简时，会有减少，便于处理�??
+     * 保存为ArrayList类型的优势在于在化简时，会有减少，便于处理。
      */
     public static ArrayList<String> getItems(String expression)
     {
@@ -340,7 +340,7 @@ public class experiment_1 {
     /**
     *
     * @param fact为多项式中的因子
-    * @return 是数字返�?1，否则返�?0�?
+    * @return 是数字返回1，否则返回0。
     */
     public static boolean isInteger(String fact)
     {
@@ -350,9 +350,9 @@ public class experiment_1 {
     
     /**
      * 
-     * @param expression:表达�?
-     * @param items:多项�? 
-     * @return 符号数组，仅含�?1
+     * @param expression:表达式
+     * @param items:多项式 
+     * @return 符号数组，仅含±1
      */
     public static int[] getSign(String expression,ArrayList<String> items)
     {
@@ -365,7 +365,7 @@ public class experiment_1 {
     		sign[j++] = 1;
     	}
     
-    	for(int i = 1;i<expression.length();i++)//处理后续多项�?
+    	for(int i = 1;i<expression.length();i++)//处理后续多项式
     	{
     		if(expression.charAt(i) == '+')
     		{
@@ -382,17 +382,17 @@ public class experiment_1 {
     
     /**
      * 判断是否为同类项
-     * @param string1 同类�?1
-     * @param string2 同类�?2
+     * @param string1 同类项1
+     * @param string2 同类项2
      * @return 是否为同类项
-     * 算法思想：将两个多项式分别按*分开后，将变量保存在两个数组里，对数组进行字典排序后比较即可。相同为同类项，不同则非同类项�??
+     * 算法思想：将两个多项式分别按*分开后，将变量保存在两个数组里，对数组进行字典排序后比较即可。相同为同类项，不同则非同类项。
      */
     public static boolean isSameItem(String string1, String string2)
     {
 		String[] factor1,factor2;
 		ArrayList<String> newFactors1 = new ArrayList<String>();
 		ArrayList<String> newFactors2 = new ArrayList<String>();
-		factor1=string1.split("\\*");//�?*号或者数字拆�?
+		factor1=string1.split("\\*");//按*号或者数字拆分
 		factor2=string2.split("\\*");
 		
 		Arrays.sort(factor1);
@@ -420,33 +420,33 @@ public class experiment_1 {
 	}
     
     /**
-     * * 对输入的expression进行�?化，输出�?化结�?
-     * items为已分开的多项式，signArr为符号数组�??
-     * �?化包括每�? 同类项系数相乘，同类项合并�??
+     * * 对输入的expression进行简化，输出简化结果
+     * items为已分开的多项式，signArr为符号数组。
+     * 简化包括每个 同类项系数相乘，同类项合并。
      * @param expression
      * @param items
      * @param signArr
-     * @return 化简后的表达�?
+     * @return 化简后的表达式
      */
     public static String makeSimple(String expression,ArrayList<String> items ,int[] signArr)
     {
         String[] factors = null;//保存多项式的因子
-        String newExpression = null;//保存�?终化�?结果
+        String newExpression = null;//保存最终化简结果
         int[] newXishu  = new int[items.size()];//化简后的系数数组
-        ArrayList<String> newItems = items;//化简后的多项�?
+        ArrayList<String> newItems = items;//化简后的多项式
         int tmpXishu = 0;//临时系数
-        String tmpItem = null;//临时多项�?
+        String tmpItem = null;//临时多项式
         
         for (int i = 0; i < newXishu.length; i++)
         {
             newXishu[i]=1;
         }
-        for (int i=0;i<newItems.size();i++)//对每个多项式内部的数字相乘化�?
+        for (int i=0;i<newItems.size();i++)//对每个多项式内部的数字相乘化简
         {
-            factors = newItems.get(i).split("\\*");//�?*分离出多项式的每个因�?
+            factors = newItems.get(i).split("\\*");//用*分离出多项式的每个因子
             for (int j = 0; j < factors.length; j++)//计算系数
             {
-                if (isInteger(factors[j]))//如果是数�?
+                if (isInteger(factors[j]))//如果是数字
                 {
                     newXishu[i] *= Integer.parseInt(factors[j]);
                 }
@@ -462,7 +462,7 @@ public class experiment_1 {
             }
             newItems.set(i, tmpItem);
         }
-        for (int i=0;i<newItems.size();i++)//合并同类�?
+        for (int i=0;i<newItems.size();i++)//合并同类项
         {
         	for(int j=i+1;j<newItems.size();j++)
         	{
@@ -471,7 +471,7 @@ public class experiment_1 {
         			tmpXishu=signArr[i]*newXishu[i]+signArr[j]*newXishu[j];//计算新的系数
         			tmpItem=Integer.toString(tmpXishu);
         			
-        			factors = newItems.get(i).split("\\*");//�?*分离出多项式的每个因�?
+        			factors = newItems.get(i).split("\\*");//用*分离出多项式的每个因子
                     for (int k = 0; k < factors.length; k++)//重新对多项式组合
                     {
                         if (!isInteger(factors[k]))//如果不是数字
@@ -479,8 +479,8 @@ public class experiment_1 {
                             tmpItem+=("*"+factors[k]);
                         }
                     }
-                    newItems.set(i, tmpItem);//更新i位置�?
-                    newItems.remove(j);//删除j位置�?
+                    newItems.set(i, tmpItem);//更新i位置项
+                    newItems.remove(j);//删除j位置项
                     
                     newXishu[i]=tmpXishu;//更新i位置系数
                     
@@ -489,7 +489,7 @@ public class experiment_1 {
                     	newXishu[l]=newXishu[l+1];//更新系数数组，j位置前移
                     }                  
                     
-                    j-=1;//删除后，新的项占了原来位置，�?要减�?，否则判断同类项会露项�??
+                    j-=1;//删除后，新的项占了原来位置，需要减一，否则判断同类项会露项。
                     
         		}
         	}
@@ -499,7 +499,7 @@ public class experiment_1 {
         }
         else{
         	newExpression=newItems.get(0);
-	        for (int i = 1; i < newItems.size(); i++) {//合成�?终化�?结果
+	        for (int i = 1; i < newItems.size(); i++) {//合成最终化简结果
 	        	newExpression += "+" + newItems.get(i);
 			}
         }
